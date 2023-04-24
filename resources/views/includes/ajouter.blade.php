86f31fd0d381fee5e77ccd0c1c6134d08c6d9971
Florent9741 <florent974;lamoly@gmail.com>
2023-04-24 11:43:21 +0200

    lundi matin

--- resources/views/includes/ajouter.blade.php
+++ resources/views/includes/ajouter.blade.php
@@ -21,7 +21,7 @@
                 x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
                 <div class="flex items-center justify-center space-x-4">
-                    <h1 class="text-4xl font-medium text-gray-800">Ajouter un nouveau film</h1>
+                    <h1 class="text-4xl font-medium text-gray-800">Ajouter un nouveau numéro</h1>
 
                     <button @click="modelOpen = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
                     </button>
